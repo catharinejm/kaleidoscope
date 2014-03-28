f75f1d14e3548c2f6ea3d2e8d3c6977f4548b1e5
Jon Distad <jon.distad@gmail.com>
2014-03-28 16:47:16 -0700

    doodads

--- doodads/testit.cc
+++ doodads/testit.cc
@@ -8,39 +8,21 @@
 
 // using namespace std;
 
+typedef int foo;
+
 class A {
-protected:
-    int x;
 public:
-    A(int i): x(i) {}
-    virtual int add(int y) { return x + y; }
-};
+    int *ip;
 
-class B : public A {
-public:
-    B(int i) : A(i) {}
-    int add(int y) { return x + y; }
-    int sub(int y) { return x - y; }
+    A() {}
 };
-
-int func(B &a) {
-    a.sub(10);
-    return a.add(40);
-}
 
 int main(int argc, char *argv[]) {
-    //GC_INIT()
-
-    B b(10);
-    // A a = b;
-    // a.add(10);
-
-    // cerr << "sizeof(void*): " << sizeof(void*) << endl;
-    // auto p = &A::add;
-    // cerr << "sizeof(&A::add): " << sizeof(&A::add) << endl;
-    // cerr << "sizeof(&p): " << sizeof(&p) << endl;
-    // cerr << "sizeof(&B::sub): " << sizeof(&B::sub) << endl;
-    // cerr << "sizeof(&func): " << sizeof(&func) << endl;
-
+    A a;
+    // if (a.ip == nullptr)
+    //     cerr << "null" << endl;
+    // else
+    //     cerr << "not null" << endl;
+    
     return 0;
 }
